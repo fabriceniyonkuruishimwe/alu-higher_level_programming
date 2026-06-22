8d93340206d04e2c7e8fb296c42c10b97678b2b9
fabriceniyonkuruishimwe <f.ishimweni@alustudent.com>
2026-06-22 00:43:31 -0700

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-word1 = "object"
-word2 = "oriented"
-word3 = "programming"
-word4 = "with"
-word5 = "Python"
-print(word1 + "-" + word2 + " " + word3 + " " + word4 + " " + word5)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112] + str[0:6])
