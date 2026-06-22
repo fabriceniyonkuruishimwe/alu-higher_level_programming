8759539271cd88afb9203a32855c5c470d8986d2
fabriceniyonkuruishimwe <f.ishimweni@alustudent.com>
2026-06-22 00:20:33 -0700

    Add 8-concat_edges.py

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+word1 = "object"
+word2 = "oriented"
+word3 = "programming"
+word4 = "with"
+word5 = "Python"
+print(word1 + "-" + word2 + " " + word3 + " " + word4 + " " + word5)
