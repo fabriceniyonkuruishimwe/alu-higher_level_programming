a213256ee295027a36102aa3fe86072cca6f61ab
fabriceniyonkuruishimwe <f.ishimweni@alustudent.com>
2026-07-09 05:54:36 -0700

    Fix 12-switch.py to match original template

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    a, b = 89, 10
-    a, b = b, a
-    print("a={} - b={}".format(a, b))
+a = 89
+b = 10
+a, b = b, a
+print("a={} - b={}".format(a, b))
