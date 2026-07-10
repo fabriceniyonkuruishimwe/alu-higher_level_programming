41f2b54fb9a4ca5eef6bbb214279d3f4d3c10996
fabriceniyonkuruishimwe <f.ishimweni@alustudent.com>
2026-07-10 01:36:15 -0700

    12. Roman to Integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return 0
+
+    values = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    total = 0
+    length = len(roman_string)
+    for i in range(length):
+        current = values[roman_string[i]]
+        if i + 1 < length and current < values[roman_string[i + 1]]:
+            total -= current
+        else:
+            total += current
+    return total
