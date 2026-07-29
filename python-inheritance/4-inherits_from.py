0f262d208da11a11946ef8d32e50c94b5d3be568
fabriceniyonkuruishimwe <f.ishimweni@alustudent.com>
2026-07-29 12:11:33 -0700

    Fix PEP8 line length in 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines a function to check if obj is inherited from a_class."""
+"""Module that defines a function to check inheritance."""
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is instance of a class that inherited from a_class."""
+    """Return True if obj is instance of a class inherited from a_class."""
     return isinstance(obj, a_class) and type(obj) is not a_class
