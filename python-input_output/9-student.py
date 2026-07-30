0d1ba16c965da80f79f7b1984d1c999d09bb0a89
fabriceniyonkuruishimwe <f.ishimweni@alustudent.com>
2026-07-30 08:51:16 -0700

    Add 9-student.py: Student class with to_json method

---
+++ python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): the student's first name.
+            last_name (str): the student's last name.
+            age (int): the student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return the dictionary description of the Student instance.
+
+        Returns:
+            dict: a dictionary representation of this instance's attributes.
+        """
+        return self.__dict__
